8b85201738057f777d3de631cf1e1452823b5088
Jos Hernandez <josue.bernal.1964@gmail.com>
2018-09-25 19:48:56 -0500

    inicio de ejercicios de js

---
+++ scripts.js
@@ -0,0 +1,80 @@
+//
+// Ejemplo de tipo de datos
+// var nombre = "Jos";// String
+// var apellido = "bernal";
+// var nombre_completo = nombre + " " + apellido;
+// var edad = 2018 - 1994;// Numero
+// var casado = false;// Buleano
+
+//console.log("mi nombre es " + nombre);
+//console.log(nombre_completo);
+//console.log(edad);
+//console.log(casado);
+
+// // Horas de sueñoptimize
+// let dormir = Number (prompt("a que hora dormiste"));
+// let despertar = Number (prompt("A que hora despertaste"));
+
+// if(dormir <= 12) {
+//   var horas_dormidas = despertar + (12 - dormir);
+// } else {
+//   var horas_dormidas = despertar +(24 - dormir);
+// }
+//
+// console.log("En total dormi " + horas_dormidas + "horas");
+
+// Ciclos -Regaño
+// for (let contador = 0; contados <= 100; contador ++) {
+//   console.log(contador);
+// }
+
+// var nombre = "Jos";
+// console.log(nombre);
+
+// if(nombre == "Jos") {
+//   console.log("Hola Jos");
+// } else if(nombre == "AlexisDoli") {
+//   console.log("Hola AlexisDoli")
+// }else {
+//   console.log("Tu no eres Anuar ni AlexisDoli");
+// }
+// Ejercicio total de supermercado
+// let Sal = Number (prompt("Cuanto costo la sal"))
+// let Azucar = Number (prompt("Cuanto costo el Azucar"))
+// let cereal = Number (prompt("Cuanto costo el cereal"))
+// let Arroz = Number (prompt("Cuanto costo el Arroz"))
+// let Helado = Number (prompt("Cuanto costo el Helado"))
+//
+// var total_supermercado = Sal + Azucar + cereal + Arroz + Helado
+//
+// console.log("El total de tu super es $" + total_supermercado +" " + "pesos");
+
+//Ejercicio de Hambre
+// let hambre = prompt("tienes hambre? (si, no)")
+// if (hambre == "si") {
+//   console.log("Estas triste")
+// } else if (hambre == "no") {
+//   console.log("Estas feliz")
+// }
+
+// Ejercicio de pesos
+let peso = Number(prompt("cuanto pesas?"))
+let peso_ideal = Number(prompt("cuanto te gustaria pesar?"))
+
+for (let contador =
+}
+//Declarando la funsión
+// function horasdesueño(){
+//   let dormir = Number (prompt("a que hora dormiste"));
+//   let despertar = Number (prompt("A que hora despertaste"));
+//
+//   if(dormir <= 12) {
+//     var horas_dormidas = despertar + (12 - dormir);
+//   } else {
+//     var horas_dormidas = despertar +(24 - dormir);
+//   }
+//
+//   console.log("En total dormi " + horas_dormidas + "horas");
+// }
+// //Ejecutar la funsión
+// hosrasdesueño();
